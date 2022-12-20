d28e0d5706d106707f6298249c83b78f21ca92b7
JakubKub13 <jakubkubala3@gmail.com>
2022-12-20 13:12:44 +0100

    Setting up constructor in Diamond

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -1,3 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import { LibDiamond } from "./libraries/LibDiamond.sol";
+import { IDiamondCut } from "./interfaces/IDiamondCut.sol";
+
+contract Diamond {
+    
+    constructor(address _ownerOfContract, address _diamondCutFacet) payable {
+        LibDiamond.setContractOwner(_ownerOfContract);
+
+        // Add external diamondCut function from diamondCutFaucet
+        IDiamondCut.FaucetCut[] memory cut = new IDiamondCut.FaucetCut[](1);
+        bytes[] memory functionSelectors = new bytes4[](1);
+        functionSelectors[0] = IDiamondCut.diamondCut.selector;
+        cut[0] = IDiamondCut.FaucetCut({
+            facetAddress: _diamondCutFacet,
+            action: IDiamondCut.FaucetCutAction.Add,
+            functionSelectors: functionSelectors
+        });
+        LibDiamond.diamondCut(cut, address(0), "");
+    }
+}
