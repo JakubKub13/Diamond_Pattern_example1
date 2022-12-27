38178a20850e29b91bd14e3f9b3c24cc44c91543
JakubKub13 <jakubkubala3@gmail.com>
2022-12-27 10:24:50 +0100

    Debuggin

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -10,7 +10,7 @@ contract Diamond {
         LibDiamond.setContractOwner(_ownerOfContract);
 
         // Add external diamondCut function from diamondCutFaucet
-        IDiamondCut.FaucetCut[] memory cut = new IDiamondCut.FaucetCut[](1);
+        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
         bytes[] memory functionSelectors = new bytes4[](1);
         functionSelectors[0] = IDiamondCut.diamondCut.selector;
         cut[0] = IDiamondCut.FaucetCut({
@@ -32,6 +32,23 @@ contract Diamond {
         address facet = ds.selectorToFacetAndPosition[msg.sig].facetAddress;
         require(facet != address(0), "Diamond: Function does not exist");
         // execute external function from facet using delegatecall and return any value.
-        
+        assembly {
+            // copy function selector and any arguments
+            calldatacopy(0, 0, calldatasize())
+            // execute function call using the facet
+            let result := delegatecall(gas(), facet, 0, calldatasize(), 0, 0)
+            // get any return value
+            returndatacopy(0, 0, returndatasize())
+            // return any return value or error back to the caller
+            switch result
+                case 0 {
+                    revert(0, returndatasize())
+                }
+                default {
+                    return(0, returndatasize())
+                }
+        }
     }
+
+    receive() external payable {}
 }
