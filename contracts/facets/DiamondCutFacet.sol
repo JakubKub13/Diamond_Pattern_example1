c9016cc65346da70ffa39548047e9e45bf0b44bf
JakubKub13 <jakubkubala3@gmail.com>
2022-12-27 10:30:28 +0100

    code refactoring

--- contracts/facets/DiamondCutFacet.sol
+++ contracts/facets/DiamondCutFacet.sol
@@ -1,2 +1,19 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import { IDiamondCut } from "../interfaces/IDiamondCut.sol";
+import { LibDiamond } from "../libraries/LibDiamond.sol";
+
+contract DiamondCutFacet is IDiamondCut {
+
+    // Loupe functions must be addred to fulfil EIP2535 diamond standart.
+
+    function diamondCut(
+        FacetCut[] calldata _diamondCut,
+        address _init,
+        bytes calldata _calldata
+    ) external override {
+        LibDiamond.enforceIsContractOwner();
+        LibDiamond.diamondCut(_diamondCut, _init, _calldata);
+    }
+}
