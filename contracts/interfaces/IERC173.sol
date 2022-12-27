36c0ab7141b08a188980f392c93ce8b33073fd55
JakubKub13 <jakubkubala3@gmail.com>
2022-12-27 08:52:27 +0100

    Adding DiamondInit

--- contracts/interfaces/IERC173.sol
+++ contracts/interfaces/IERC173.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.0;
 
 interface IERC173 {
-    
+
+    // erc-165 identifier for this interface is 0x7f5828d0
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     function owner() external view returns (address owner_);
--- contracts/upgradeInitializers/DiamondInit.sol
+++ contracts/upgradeInitializers/DiamondInit.sol
@@ -1,2 +1,36 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import { LibDiamond } from "../libraries/LibDiamond.sol";
+import { IDiamondLoupe } from "../interfaces/IDiamondLoupe.sol";
+import { IDiamondCut } from "../interfaces/IDiamondCut.sol";
+import { IERC173 } from "../interfaces/IERC173.sol";
+import { IERC165 } from "../interfaces/IERC165.sol";
+
+
+contract DiamondInit {
+
+
+    // Params could be passed here in order to pass them to set up own state variables
+     // add your own state variables 
+        // EIP-2535 specifies that the `diamondCut` function takes two optional 
+        // arguments: address _init and bytes calldata _calldata
+        // These arguments are used to execute an arbitrary function using delegatecall
+        // in order to set state variables in the diamond during deployment or an upgrade
+        // More info here: https://eips.ethereum.org/EIPS/eip-2535#diamond-interface 
+    function init() external {
+        // adding ERC165 data
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        ds.supportedInterfaces[type(IERC165).interfaceId] = true;
+        ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
+        ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC173).interfaceId] = true;
+
+        //  add your own state variables 
+        // EIP-2535 specifies that the `diamondCut` function takes two optional 
+        // arguments: address _init and bytes calldata _calldata
+        // These arguments are used to execute an arbitrary function using delegatecall
+        // in order to set state variables in the diamond during deployment or an upgrade
+        // More info here: https://eips.ethereum.org/EIPS/eip-2535#diamond-interface 
+    }
+}
